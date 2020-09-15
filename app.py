e87dcf9dcb8079802bc5de59ad1f717e50eed43c
Elektra Babian <babianelektra@gmail.com>
2020-09-15 17:39:28 -0400

    accessed the ingredients api and printed the data from the response

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import requests
+import json
 
 url = "https://raw.githubusercontent.com/daily-harvest/opportunities/master/web-1/data/ingredients.json"
 headers = {
-        'Content-Type': "application/x-www-form-urlencoded",
+        'Content-Type': "application/json",
         'Cache-Control': "no-cache"
         }
 
-response = requests.request("GET", url, headers=headers)
+response = requests.get(url, headers=headers)
 
-print(response)
+data = response.json()
+
+print(data)
 
