ed8749515749db6a19cff4803903cd20d98cdbf1
Elektra Babian <babianelektra@gmail.com>
2020-09-15 18:17:26 -0400

    installed flask and flask_restful. created ingredient class and route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
-import json
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
 
 url = "https://raw.githubusercontent.com/daily-harvest/opportunities/master/web-1/data/ingredients.json"
 headers = {
@@ -13,3 +14,16 @@ data = response.json()
 
 print(data)
 
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+
+class IngredientList(Resource):
+        def get(self):
+                return data
+
+api.add_resource(IngredientList, '/ingredients/')
+
+if __name__ == '__main__':
+        app.run(debug=True)
