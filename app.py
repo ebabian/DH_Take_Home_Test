95f957733e9c14b6916f25023dd1003a5f3dd848
Elektra Babian <babianelektra@gmail.com>
2020-09-16 14:35:29 -0400

    listed products in browser

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
 import requests
-from flask import Flask, render_template
-from flask_restful import Resource, Api
+from flask import Flask, render_template, request
 
-url = "https://raw.githubusercontent.com/daily-harvest/opportunities/master/web-1/data/ingredients.json"
-headers = {
-    'Content-Type': "application/json",
-    'Cache-Control': "no-cache"
-}
-
-response = requests.get(url, headers=headers)
-
-data = response.json()
-
-for ingredient in data:
-        print (ingredient['id'])
-
-# print(data)
+# from flask_restful import Resource, Api
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
 
-# class IngredientList(Resource):
-# def get(self):
-#       return data
-
-# api = Api(app)
 
 # http://localhost:5000/
 @app.route('/', methods=['GET'])
-def home():
-    return render_template('index.html')
+def index():
+    ingredient_url = "https://raw.githubusercontent.com/daily-harvest/opportunities/master/web-1/data/ingredients.json"
+    product_url = "https://raw.githubusercontent.com/daily-harvest/opportunities/master/web-1/data/products.json"
+
+    headers = {
+        'Content-Type': "application/json",
+        'Cache-Control': "no-cache"
+    }
+
+    products = []
+
+    response = requests.get(ingredient_url)
+
+    data = response.json()
+
+    ingredient_ids = []
+    for ingredient in data:
+        ingredient_ids.append(ingredient['id'])
+        print(ingredient_ids)
+
+    product_response = requests.get(product_url)
+    results = product_response.json()
+    for product in results:
+        product_data = {
+            'id' : product['id'],
+            'name' : product['name']
+        }
+        products.append(product_data)
+
+    return render_template('index.html', products=products)
 
 # Route: http://127.0.0.1:5000/ingredients/
 # api.add_resource(IngredientList, '/ingredients/')
@@ -37,4 +45,5 @@ def home():
 # if __name__ == '__main__':
 # app.run(debug=True)
 
+app.config["DEBUG"] = True
 app.run()
